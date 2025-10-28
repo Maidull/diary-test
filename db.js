a2aaa4d9d2de19198854cddad885e03284f93f35
Mai <mainguyen2913@gmail.com>
2025-10-29 01:45:44 +0700

    edit db name

--- db.js
+++ db.js
@@ -4,26 +4,14 @@ const db = mysql.createConnection({
   host: "mai-db.cvq02qggo42p.ap-northeast-1.rds.amazonaws.com",  
   user: "admin",            
   password: "12345678", 
-  database: "mai-db",         
+  database: "mai",         
 });
 
 db.connect((err) => {
   if (err) {
-    console.error("❌ Lỗi kết nối RDS:", err);
+    console.error("❌ Kết nối RDS thất bại:", err);
   } else {
-    console.log("✅ Đã kết nối thành công tới RDS MySQL (mai-db)");
-    const createTableQuery = `
-      CREATE TABLE IF NOT EXISTS diary (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        title VARCHAR(255),
-        content TEXT,
-        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-      )
-    `;
-    db.query(createTableQuery, (err) => {
-      if (err) console.error("❌ Lỗi tạo bảng diary:", err);
-      else console.log("✅ Bảng diary đã sẵn sàng!");
-    });
+    console.log("✅ Đã kết nối thành công tới RDS MySQL");
   }
 });
 
